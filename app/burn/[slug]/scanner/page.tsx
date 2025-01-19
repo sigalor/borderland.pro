6dbf0abbea88781161b1a4dff240893e8ea633ce
henophilia <tech@henophilia.org>
2025-01-19 10:04:45 +0100

    feat: add placeholder for membership scanner

---
+++ app/burn/[slug]/scanner/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Heading from "@/app/_components/Heading";
+import { Button } from "@nextui-org/react";
+
+export default function ScannerPage() {
+  return (
+    <div>
+      <Heading>Membership scanner</Heading>
+      <Button isDisabled>Launch scanner (not implemented yet)</Button>
+    </div>
+  );
+}
